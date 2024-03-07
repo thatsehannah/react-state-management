bffcaf02ed1be37e58c50951f41bff3c030f0d83
E. Hannah <echannah631@gmail.com>
2024-03-07 08:24:06 -0600

    created app context and context provider, implemented app context in header component (not functional)

--- src/App.tsx
+++ src/App.tsx
@@ -4,76 +4,11 @@ import { Main } from './Main';
 import { authenticate, User } from './api/authenticate';
 import { authorize } from './api/authorize';
 
-type State = {
-  user: undefined | User;
-  permissions: undefined | string[];
-  loading: boolean;
-};
-
-const initialState: State = {
-  user: undefined,
-  permissions: undefined,
-  loading: false,
-};
-
-type Action =
-  | {
-      type: 'authenticate';
-    }
-  | {
-      type: 'authenticated';
-      user: User | undefined;
-    }
-  | {
-      type: 'authorize';
-    }
-  | {
-      type: 'authorized';
-      permissions: string[];
-    };
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case 'authenticate':
-      return { ...state, loading: true };
-    case 'authenticated':
-      return { ...state, loading: false, user: action.user };
-    case 'authorize':
-      return { ...state, loading: true };
-    case 'authorized':
-      return { ...state, loading: false, permissions: action.permissions };
-    default:
-      return state;
-  }
-};
-
 const App = () => {
   const [{ user, permissions, loading }, dispatch] = useReducer(
     reducer,
     initialState
   );
-
-  const handleSignInClick = async () => {
-    dispatch({ type: 'authenticate' });
-
-    const authenticatedUser = await authenticate();
-
-    dispatch({
-      type: 'authenticated',
-      user: authenticatedUser,
-    });
-
-    if (authenticatedUser !== undefined) {
-      dispatch({ type: 'authorize' });
-
-      const authorizedPermissions = await authorize(authenticatedUser.id);
-
-      dispatch({
-        type: 'authorized',
-        permissions: authorizedPermissions,
-      });
-    }
-  };
 
   return (
     <div className='max-w-7xl mx-auto px-4'>
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,19 +1,39 @@
-import { User } from './api/authenticate';
+import { authenticate } from './api/authenticate';
+import { authorize } from './api/authorize';
+import { useAppContext } from './AppContext';
 
-type Props = {
-  user: undefined | User;
-  onSignInClick: () => void;
-  loading: boolean;
-};
+export const Header = () => {
+  const { user, loading, dispatch } = useAppContext();
 
-export const Header = ({ user, onSignInClick, loading }: Props) => {
+  const handleSignInClick = async () => {
+    dispatch({ type: 'authenticate' });
+
+    const authenticatedUser = await authenticate();
+
+    dispatch({
+      type: 'authenticated',
+      user: authenticatedUser,
+    });
+
+    if (authenticatedUser !== undefined) {
+      dispatch({ type: 'authorize' });
+
+      const authorizedPermissions = await authorize(authenticatedUser.id);
+
+      dispatch({
+        type: 'authorized',
+        permissions: authorizedPermissions,
+      });
+    }
+  };
+
   return (
     <header className='flex justify-between items-center border-b-2 border-gray-100 py-6'>
       {user ? (
         <span className='ml-auto font-bold'>{user.name} has signed in</span>
       ) : (
         <button
-          onClick={onSignInClick}
+          onClick={handleSignInClick}
           className='whitespace-nowrap inline-flex items-center justify-center ml-auto px-4 py-2 w-36 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'
         >
           {loading ? '....' : 'Sign in'}
